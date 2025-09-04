9897d093ced43b873c245cd629e1f1fe9c9c22cd
avcopan <avcopan@gmail.com>
2025-09-04 15:42:30 -0500

    Verifies cyC5e1 inner saddle point by dirty IRC

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -4,8 +4,4 @@ WD=${INIT_CWD:-$(pwd)}
 DIR=${1}
 
 cd ${WD}
-cp ${DIR}/*.inp .
-cp ${DIR}/*.xyz .
-cp ${DIR}/*.allxyz .
-cp ${DIR}/*.log .
-cp ${DIR}/*.ipynb .
+cp ${DIR}/*.{inp,xyz,allxyz,log,ipynb,hess} .
